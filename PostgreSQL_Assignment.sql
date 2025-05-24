99d3f5a39243bb00b45e6c9d0cdb9439ad7b6bfd
unknown <prantokih42@gmail.com>
2025-05-24 21:11:41 +0600

    ramger table created

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,1 +1,11 @@
 -- Active: 1747555016962@@localhost@5000@conservation_db@public
+SELECT * FROM rangers
+
+CREATE TABLE rangers (
+    ranger_id SERIAL PRIMARY KEY,
+    "name" VARCHAR(50) NOT NULL,
+    region VARCHAR(50) NOT NULL
+ 
+)
+
+INSERT INTO rangers ("name", region) VALUES ('Red Ranger', 'Region A')
