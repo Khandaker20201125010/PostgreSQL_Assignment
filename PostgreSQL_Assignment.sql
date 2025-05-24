a4523338d677b65302febddb8aedda295a8c35b5
unknown <prantokih42@gmail.com>
2025-05-24 22:16:39 +0600

    relation between two table added

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,5 +1,5 @@
 -- Active: 1747555016962@@localhost@5000@conservation_db@public
-SELECT * FROM rangers
+
 
 CREATE TABLE rangers (
     ranger_id SERIAL PRIMARY KEY,
@@ -8,4 +8,48 @@ CREATE TABLE rangers (
  
 )
 
-INSERT INTO rangers ("name", region) VALUES ('Red Ranger', 'Region A')
+INSERT INTO rangers ("name", region) VALUES 
+('Red Ranger', 'Region A'),
+('Blue Ranger', 'Region B'),
+('Green Ranger', 'Region C')
+
+SELECT * FROM rangers
+
+CREATE TABLE species (
+    species_id SERIAL PRIMARY KEY,
+    common_name VARCHAR(50) NOT NULL,
+    scientific_name VARCHAR(50) NOT NULL,
+    discovery_date DATE NOT NULL,
+    conservation_status VARCHAR(50)
+)
+
+INSERT INTO species (common_name, scientific_name, discovery_date, conservation_status) VALUES 
+('Red Panda', 'Ailurus fulgens', '2025-05-24', 'Endangered'),
+('Blue Whale', 'Balaenoptera musculus', '2025-05-24', 'Endangered'),
+('Green Turtle', 'Chelonia mydas', '2025-05-24', 'Endangered')
+
+
+SELECT * FROM species
+
+CREATE TABLE sightings (
+    sighting_id SERIAL PRIMARY KEY,
+    ranger_id INT NOT NULL REFERENCES rangers(ranger_id),
+    species_id INT NOT NULL REFERENCES species(species_id),
+    sighting_time TIMESTAMP NOT NULL,
+    location TEXT NOT NULL,
+    note TEXT
+)
+
+INSERT INTO sightings (sighting_id, ranger_id, species_id, sighting_time, location, note) VALUES
+(1, 1, 1, '2025-05-24 10:00:00', 'Panda Forest', 'A group of pandas were sighted in the forest'),
+(2, 2, 2, '2025-05-24 11:00:00', 'Ocean Bay', 'A blue whale was observed near the bay'),
+(3, 3, 3, '2025-05-24 12:00:00', 'Coral Reef', 'Green turtles nesting on the beach');
+
+
+
+
+
+DROP TABLE sightings
+
+SELECT * FROM sightings
+
